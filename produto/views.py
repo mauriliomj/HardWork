5373c9ec010448be1ea97b0c5e1214af32705f96
maurilio <mauriliojose249@gmail.com>
2022-11-14 12:52:43 -0300

    Criando templates e paginação

--- produto/views.py
+++ produto/views.py
@@ -4,10 +4,13 @@ from django.views import View
 from django.http import HttpResponse
 from . import models
 
+
 class ListaProdutos(ListView):
     model = models.Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
+    paginate_by = 10
+
 
 class DetalheProduto(View):
     def get(self , *args , **Kwargs):
